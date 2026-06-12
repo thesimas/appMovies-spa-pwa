66923e004d1a50bd2dcf06a5ed799f8d8d42628f
thesimas <luciano.simasjr@gmail.com>
2026-06-12 13:18:39 -0300

    Adicionando os arquivos Service Worker e manifest para que o usuario possa instalar o aplicativo e poder rodar ele offline, além de adicionar a opção de baixar o app no html e fazer a logica no js, códigos providos do APP MINHA LOJA

--- app.js
+++ app.js
@@ -225,3 +225,19 @@ async function mostrarDetalhes(idFilme) {
         console.log(`Erro na requisição: ${erro}`);
     }
 }
+
+if ('serviceWorker' in navigator) {  
+    navigator.serviceWorker.register("./service-worker.js");
+}
+
+var pedidoInstalacao;
+window.addEventListener('beforeinstallprompt', function(installPrompt) {
+    if(installPrompt){
+        document.getElementById("installAppBt").classList.add('show')
+        pedidoInstalacao = installPrompt
+    }
+});
+
+function installApp() {
+    pedidoInstalacao.prompt(); 
+}
