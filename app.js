3814ceaa091f62b5823d67d350c7808d16a23215
thesimas <luciano.simasjr@gmail.com>
2026-06-28 11:32:24 -0300

    Add: Lógica para adicionar e remover filmes favoritos no localStorage, é uma forma de persistir um dado no proprio navegador do cliente, sem envolver banco de dados e backend.

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ async function listarFilmesPorCategorias(idCategoria) {
         const response = await axios.get(url);
         const select = document.getElementById('select-categorias');
         select.selectedIndex = 0;
-        
+
         titulo = `Filmes de ${categoriaGlobais[idCategoria]}`;
         setTitulo(titulo);
 
@@ -114,7 +114,7 @@ async function listarFilmesPopulares() {
         const telaLista = document.getElementById("tela-principal");
         const url = `${urlBase}/3/movie/popular?api_key=${chaveAPI}&language=pt-BR`;
         const response = await axios.get(url);
-        
+
         titulo = "Filmes Populares";
         setTitulo(titulo);
 
@@ -129,6 +129,7 @@ async function listarFilmesPopulares() {
             filme.genre_ids.forEach(genero => {
                 nomeCategorias += `${categoriaGlobais[genero]} <br>`;
             });
+            
             // Separa a data pelo traço, inverte o array e depois junta ele usando / para separar. 
             const dataFormatada = filme.release_date.split('-').reverse().join('/');
 
@@ -158,7 +159,7 @@ async function listarFilmesPopulares() {
     }
 }
 
-function setTitulo(tituloNovo){
+function setTitulo(tituloNovo) {
     const titulo = document.getElementById('titulo');
 
     tituloAnterior = tituloAtual;
@@ -173,7 +174,7 @@ async function lancamentos() {
         const telaLista = document.getElementById("tela-principal");
         const url = `${urlBase}/3/movie/upcoming?api_key=${chaveAPI}&language=pt-BR`;
         const response = await axios.get(url);
-        
+
         titulo = "Filmes Lançamentos";
         setTitulo(titulo);
 
@@ -287,6 +288,131 @@ async function buscar(inputUsuario) {
     }
 }
 
+function favoritar(idFilme) {
+
+    try {
+        let lista = localStorage.getItem('filmesFavoritos');
+
+        // Se a lista vier preenchida, transformamos ela em uma json, se não ela vira um array. 
+        let listaFilmesFavoritos = lista ? JSON.parse(lista) : [];
+
+        if (!listaFilmesFavoritos.includes(idFilme)) {
+
+            listaFilmesFavoritos.push(idFilme);
+
+            // Salva a lista atualizada no Local Storage, transformando ela em uma string.
+            localStorage.setItem('filmesFavoritos', JSON.stringify(listaFilmesFavoritos));
+
+            alert("Filme salvo com sucesso!");
+        } else {
+            alert("Filme já está na sua lista de filmes favoritos!");
+        }
+
+    } catch (erro) {
+        console.log("Erro ao salvar o filme no Local Storage: ", erro);
+    }
+}
+
+function removerFilmeFavorito(idFilme) {
+    try {
+        // Recupera a lista de filmes favoritos do Local Storage
+        let listaString = localStorage.getItem('filmesFavoritos');
+
+        // Se a lista existir, converte para array, caso contrário, cria um array vazio
+        let listaFilmesFavoritos = listaString ? JSON.parse(listaString) : [];
+
+        // Filtra a lista removendo o filme com o id fornecido
+        listaFilmesFavoritos = listaFilmesFavoritos.filter(id => id !== idFilme);
+        
+        localStorage.setItem('filmesFavoritos', JSON.stringify(listaFilmesFavoritos));
+
+        alert("Filme removido dos favoritos.");
+
+    } catch (erro) {
+        console.log("Erro ao remover o filme no Local Storage: ", erro);
+    }
+}
+
+function verificarSeFilmeFavorito(idFilme) {
+    let listaString = localStorage.getItem('filmesFavoritos');
+
+    let listaFilmesFavoritos = listaString ? JSON.parse(listaString) : [];
+
+    if(listaFilmesFavoritos.includes(idFilme)) {
+        return true;
+    } else {
+        return false;
+    }
+}    
+
+async function listarFilmesFavoritos() {
+    try {
+        const telaLista = document.getElementById("tela-principal");
+        telaLista.innerHTML = "";
+        
+        titulo = "Meus Filmes Favoritos";
+        setTitulo(titulo);
+
+        let listaString = localStorage.getItem('filmesFavoritos');
+
+        let listaFilmesFavoritos = listaString ? JSON.parse(listaString) : [];
+
+        
+        if (listaFilmesFavoritos.length === 0) {
+            telaLista.innerHTML = "<p class='text-center w-100 fs-5 mt-4 text-muted'>Você ainda não tem filmes favoritos salvos.</p>";
+        } else {
+            
+            for (const id of listaFilmesFavoritos) {
+                
+                
+                let url = `${urlBase}/3/movie/${id}?api_key=${chaveAPI}&language=pt-BR`;
+
+                const response = await axios.get(url);
+                
+                
+                const filme = response.data; 
+
+                const card = document.createElement('div');
+                card.classList.add('col');
+
+                let nomeCategorias = "";
+
+                filme.genres.forEach(genero => {
+                    nomeCategorias += `${genero.name}<br>`;
+                });
+
+                const dataFormatada = filme.release_date.split('-').reverse().join('/');
+
+                card.innerHTML = `
+                <div class="card h-100 shadow-sm border-0" onclick="mostrarDetalhes(${filme.id})" style="cursor: pointer;">
+                    <img src="https://image.tmdb.org/t/p/w500/${filme.poster_path}" class="card-img-top" alt="Imagem do Filme" style="aspect-ratio: 2/3; object-fit: cover;">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title fw-bold">${filme.title}</h5>
+                        <p class="card-text mb-2 text-muted" style="font-size: 0.9rem;">Lançamento: ${dataFormatada}</p>
+                        <div class="mt-auto">
+                            <span style="font-size: 0.85rem;" class="fw-semibold text-primary">${nomeCategorias}</span>
+                        </div>
+                    </div>
+                </div>`;
+
+                telaLista.appendChild(card);
+            }
+        }
+        
+        navegar("tela-principal"); 
+        
+        // Fecha o menu sanduíche ao clicar em "Meus Favoritos"
+        const menuSanduiche = document.getElementById('navbarNav');
+        if (menuSanduiche.classList.contains('show')) {
+            const menuCollapse = bootstrap.Collapse.getOrCreateInstance(menuSanduiche);
+            menuCollapse.hide();
+        }
+
+    } catch (erro) {
+        console.log(`Erro na requisição: ${erro}`);
+    }
+}
+
 async function mostrarDetalhes(idFilme) {
 
     try {
@@ -307,6 +433,9 @@ async function mostrarDetalhes(idFilme) {
         });
 
         const dataFormatada = filme.release_date.split('-').reverse().join('/');
+        
+        // Operador ternario para verificar se o filme está nos favoritos e exibir o botão correspondente.
+        const botao = verificarSeFilmeFavorito(idFilme) ? `<button class="btn btn-outline-danger px-5 py-2 shadow-sm rounded-pill" onclick="removerFilmeFavorito(${idFilme})">Remover dos favoritos</button>` : `<button class="btn btn-outline-secondary px-5 py-2 shadow-sm rounded-pill" onclick="favoritar(${idFilme})">Adicionar aos favoritos</button>`;
 
         card.innerHTML = `
             <div class="col-md-3 text-center mb-4 mb-md-0">
@@ -336,6 +465,8 @@ async function mostrarDetalhes(idFilme) {
                 <div class="text-center mt-4">
                     <button class="btn btn-outline-secondary px-5 py-2 shadow-sm rounded-pill" onclick="voltar()">
                         Voltar para a lista
+                    </button>
+                    ${botao}
                     </button>
                 </div>
             </div>
